f754798b0afb756458c72c9f007f26976e736408
Sawyer Hood <sawyerjhood@gmail.com>
2024-06-18 13:03:21 -0700

    Generate name

---
+++ app/api/name/route.ts
@@ -0,0 +1,35 @@
+import { MODEL, openai } from "@/ai/client";
+import { extractXMLTag } from "@/lib/extractXMLTag";
+
+export async function POST(req: Request) {
+  const { desc } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: MODEL,
+    messages: [
+      {
+        role: "system",
+        content: prompt,
+      },
+      {
+        role: "user",
+        content: desc,
+      },
+    ],
+    max_tokens: 4000,
+  });
+
+  console.log(response);
+
+  const content = response.choices[0].message.content;
+
+  const name = extractXMLTag(content!, "appname");
+
+  return new Response(JSON.stringify({ name }), { status: 200 });
+}
+
+const prompt = `You are an expert application namer. The user will give you a description
+of an application and you will create a simple name for it. These applications are for the
+Windows96 operating system, a retrofuturistic operating system. make the names creative and 
+whimsical. Put the name in <appname> tags.
+`;
--- components/programs/Run.tsx
+++ components/programs/Run.tsx
@@ -3,28 +3,43 @@ import { useSetAtom } from "jotai";
 import { windowsListAtom } from "@/state/windowsList";
 import { createWindow } from "../../lib/createWindow";
 import { ProgramEntry, programsAtom } from "@/state/programs";
+import { useState } from "react";
 
 export function Run({ id }: { id: string }) {
   const windowsDispatch = useSetAtom(windowsListAtom);
   const programsDispatch = useSetAtom(programsAtom);
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <form
       style={{ display: "flex", flexDirection: "column", gap: 8 }}
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
+        if (isLoading) return;
+        setIsLoading(true);
         const formData = new FormData(e.currentTarget);
         const programDescription = formData.get("program-description");
         if (typeof programDescription === "string") {
+          let name = programDescription;
+
+          if (name.length > 20) {
+            const nameResp = await fetch("/api/name", {
+              method: "POST",
+              body: JSON.stringify({ desc: programDescription }),
+            });
+
+            name = (await nameResp.json()).name;
+          }
+
           const program: ProgramEntry = {
             id: crypto.randomUUID(),
             prompt: programDescription,
-            name: programDescription,
+            name,
           };
 
           programsDispatch({ type: "ADD_PROGRAM", payload: program });
 
           createWindow({
-            title: programDescription,
+            title: name,
             program: {
               type: "iframe",
               programID: program.id,
@@ -56,7 +71,9 @@ export function Run({ id }: { id: string }) {
         />
       </div>
       <div style={{ display: "flex", gap: 8, justifyContent: "flex-end" }}>
-        <button type="submit">Open</button>
+        <button type="submit" disabled={isLoading}>
+          Open
+        </button>
         <button
           onClick={() => windowsDispatch({ type: "REMOVE", payload: id })}
         >
