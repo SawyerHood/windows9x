dd86f48ba27051116cab936cdf46857a35c3d2ef
Sawyer Hood <sawyerjhood@gmail.com>
2024-06-19 18:43:35 -0700

    Make fonts more readable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import "98.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
