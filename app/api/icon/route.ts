c26f6a7f00bcaa6bd349f8f00bb2648558ee142b
Sawyer Hood <sawyerjhood@gmail.com>
2024-07-02 11:34:13 -0700

    Remove cloudflare code

--- app/api/icon/route.ts
+++ app/api/icon/route.ts
@@ -62,28 +62,3 @@ async function genImagePrompt(name: string, settings: Settings) {
 
   return result.choices[0].message.content;
 }
-
-// async function genCloudflareImage(prompt: string): Promise<Blob | null> {
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "image/jpeg",
-//       Authorization: `Bearer ${process.env.CLOUDFLARE_TOKEN}`,
-//     },
-//     body: JSON.stringify({ prompt }),
-//   };
-
-//   const resp = await fetch(
-//     `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/bytedance/stable-diffusion-xl-lightning`,
-//     options
-//   );
-
-//   if (!resp.ok) {
-//     console.error("Error generating image", await resp.text());
-//     console.error("resp", resp);
-//     return null;
-//   }
-
-//   return await resp.blob();
-// }
