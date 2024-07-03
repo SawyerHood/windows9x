32aaf66377116f37b3c2db46ef4c488c75dd653a
Sawyer Hood <sawyerjhood@gmail.com>
2024-07-02 21:33:29 -0700

    Startup sound

--- components/programs/Welcome.tsx
+++ components/programs/Welcome.tsx
@@ -1,4 +1,13 @@
 "use client";
+
+import { useEffect } from "react";
+
 export function Welcome({ id: _id }: { id: string }) {
+  useEffect(() => {
+    const audio = new Audio("/start.mp3");
+    audio.play().catch((error) => {
+      console.error("Error playing start.mp3:", error);
+    });
+  }, []);
   return <div>Welcome to Windows 9X</div>;
 }
