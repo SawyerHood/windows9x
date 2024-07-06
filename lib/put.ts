db5ca1d5f6d0afd5eb3e726fd5b2f1f7ad275cb8
Sawyer Hood <sawyerjhood@gmail.com>
2024-07-06 08:55:55 -0700

    reenable saving files locally

--- lib/put.ts
+++ lib/put.ts
@@ -2,7 +2,13 @@ import { createClient } from "./supabase/server";
 
 export async function put(path: string, blob: Blob): Promise<string> {
   if (process.env.LOCAL_MODE) {
-    return "";
+    const fs = await import("fs-extra");
+
+    const buffer = await blob.arrayBuffer();
+    const data = Buffer.from(buffer);
+    await fs.outputFile(`${process.cwd()}/public/blob/${path}`, data);
+
+    return `http://localhost:3000/blob/${path}`;
   }
   const supabase = createClient();
 
