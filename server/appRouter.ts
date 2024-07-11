ad485e0b6820f2ca61e29399f7ab75dda0ab7d86
Sawyer Hood <sawyerjhood@gmail.com>
2024-07-10 17:47:32 -0700

    Tokens work for local mode

--- server/appRouter.ts
+++ server/appRouter.ts
@@ -3,9 +3,15 @@ import { publicProcedure, router } from "./trpc";
 import { getUser } from "@/lib/auth/getUser";
 import { TRPCError } from "@trpc/server";
 import { getTokens } from "./usage/getTokens";
+import { isLocal } from "@/lib/isLocal";
 
 export const appRouter = router({
   getTokens: publicProcedure.query(async () => {
+    if (isLocal()) {
+      return {
+        tokens: 1000,
+      };
+    }
     const client = createClient();
     const user = await getUser();
     if (!user) {
